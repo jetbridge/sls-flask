b990a24c83b49891ee47647e9a029446b93f49f4
Mischa Spiegelmock <mischa@mvstg.biz>
2020-01-05 14:40:14 +0200

    lint

--- TEMPLATE/api/auth/__init__.py
+++ TEMPLATE/api/auth/__init__.py
@@ -1,4 +1,4 @@
-from jetkit.api.auth import  blp, use_core_auth_api  # noqa: F401
+from jetkit.api.auth import blp, use_core_auth_api  # noqa: F401
 from TEMPLATE.model.user import User
 from .schema import UserSchema
 
