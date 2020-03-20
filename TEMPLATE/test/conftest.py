d48886b658cd84549400a707a31a69738c79c1b7
Mischa Spiegelmock <me@mish.dev>
2020-03-20 14:32:40 +0200

    cleanup

--- TEMPLATE/test/conftest.py
+++ TEMPLATE/test/conftest.py
@@ -17,9 +17,9 @@ LOCALE = "en_US"
 
 # Retrieve a database connection string from the environment
 # should be a DB that doesn't exist
-DB_CONN = os.getenv("TEST_DATABASE_URL", "postgresql:///TEMPLATE_test".lower())
+DB_CONN = os.getenv("TEST_DATABASE_URI", "postgresql:///TEMPLATE_test".lower())
 DB_OPTS = sa.engine.url.make_url(DB_CONN).translate_connect_args()
-DB_VERSION = "10.10"
+DB_VERSION = "11.5"
 
 
 @pytest.fixture(scope="session")
